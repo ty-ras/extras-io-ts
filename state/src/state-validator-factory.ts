ccc52a059b4ce68936d173b5cb68ded77ec374fd
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-01-11 22:54:25 +0200

    #17 Adding code after testing it out in sample, a…
    …nd splitting it into more understandable fragments.

--- state/src/state-validator-factory.ts
+++ state/src/state-validator-factory.ts
@@ -2,23 +2,20 @@ import * as t from "io-ts";
 import { function as F, either as E } from "fp-ts";
 import type * as tyrasEP from "@ty-ras/endpoint";
 import * as tyras from "@ty-ras/data-io-ts";
-import * as tyrasData from "@ty-ras/data";
+import type * as validation from "./validation";
+import type * as state from "./state";
+import type * as stateInfo from "./state-info";
+import * as spec from "./spec";
 
 export const createStateValidatorFactory =
-  <TStateValidation extends TStateValidationBase>(
+  <TStateValidation extends validation.TStateValidationBase>(
     validation: TStateValidation,
-  ) =>
-  <TStateSpec extends StateSpec<TStateValidation>>(
-    spec: TStateSpec,
-  ): tyrasEP.EndpointStateValidator<
-    StateInfoOfKeys<keyof TStateSpec>,
-    GetState<TStateValidation, TStateSpec>
-    // eslint-disable-next-line sonarjs/cognitive-complexity
-  > => {
-    const entries = Object.entries(spec) as Array<
+  ): CreateStateValidatorFactory<TStateValidation> =>
+  (specObject) => {
+    const entries = Object.entries(specObject) as Array<
       [
-        keyof TStateSpec & keyof TStateValidation,
-        StatePropertySpec<TStateValidation>,
+        keyof typeof specObject & keyof TStateValidation,
+        spec.StatePropertySpec<TStateValidation>,
       ]
     >;
     const getValidator = (
@@ -34,19 +31,20 @@ export const createStateValidatorFactory =
           doThrow(`State does not contain "${String(propName)}".`)
         );
       } else {
+        // For more complex spec, see what is being specified and return validation based on that.
         switch (propSpec.match) {
-          case MATCH_EXACT: {
+          case spec.MATCH_EXACT: {
             const matched = propSpec.value;
             return t.refinement(t.unknown, (v) => v === matched);
           }
-          case MATCH_ONE_OF: {
+          case spec.MATCH_ONE_OF: {
             const oneOf = propSpec.values;
             return t.refinement(t.UnknownArray, (arr) =>
               oneOf.some((one) => arr.some((v) => v === one)),
             );
           }
-          case MATCH_ALL_OF: {
-            const allOf = propSpec.values as ReadonlyArray<unknown>;
+          case spec.MATCH_ALL_OF: {
+            const allOf = propSpec.values;
             return t.refinement(t.UnknownArray, (arr) =>
               allOf.every((one) => arr.some((v) => v === one)),
             );
@@ -107,7 +105,10 @@ export const createStateValidatorFactory =
             // In case of success, transform it into DataValidationResponseSuccess
             (result) => ({
               error: "none" as const,
-              data: result as GetState<TStateValidation, TStateSpec>,
+              data: result as state.GetState<
+                TStateValidation,
+                typeof specObject
+              >,
             }),
           ),
           // "Merge" the result of previous operation as TyRAS operates on type unions, not either-or constructs.
@@ -116,98 +117,21 @@ export const createStateValidatorFactory =
     };
   };
 
-export const getFullStateValidationInfo = <
-  TAuthenticated extends Record<string, t.Mixed>,
-  TOther extends Record<string, t.Mixed>,
->(
-  authenticated: TAuthenticated,
-  other: TOther,
-) =>
-  ({
-    ...tyrasData.transformEntries(authenticated, (validation) => ({
-      validation,
-      isAuthenticationProperty: true,
-    })),
-    ...tyrasData.transformEntries(other, (validation) => ({
-      validation,
-      isAuthenticationProperty: false,
-    })),
-  } as {
-    [P in keyof TAuthenticated]: StatePropertyValidation<
-      TAuthenticated[P],
-      true
-    >;
-  } & { [P in keyof TOther]: StatePropertyValidation<TOther[P], false> });
+export type CreateStateValidatorFactory<
+  TStateValidation extends validation.TStateValidationBase,
+> = <TStateSpec extends spec.StateSpec<TStateValidation>>(
+  spec: TStateSpec,
+) => tyrasEP.EndpointStateValidator<
+  stateInfo.StateInfoOfKeys<keyof TStateSpec>,
+  state.GetState<TStateValidation, TStateSpec>
+>;
 
-export type TStateValidationBase = Record<string, StatePropertyValidation>;
-
-export interface StatePropertyValidation<
-  TValidation extends t.Mixed = t.Mixed,
-  TIsAuthentication extends boolean = boolean,
-> {
-  validation: TValidation;
-  isAuthenticationProperty: TIsAuthentication;
-}
-
-export type StateInfo<TState> = StateInfoOfKeys<keyof TState>;
-export type StateInfoOfKeys<TKeys extends PropertyKey> = ReadonlyArray<TKeys>;
-
-export type StateSpec<TStateValidation extends TStateValidationBase> = {
-  readonly [P in string]: keyof TStateValidation extends P
-    ? StatePropertySpec<t.TypeOf<TStateValidation[P]["validation"]>>
-    : never;
-};
-export type StatePropertySpec<T> = T extends boolean
-  ? // Only allow match specs
-    StatePropertyMatchSpec<T>
-  : // Allow simple spec + match specs
-    StatePropertySpecAll<T>;
-
-export type StatePropertySpecAll<T> =
-  | StatePropertyRequiredSpec
-  | StatePropertyMatchSpec<T>;
-export type StatePropertyRequiredSpec = boolean;
-export type StatePropertyMatchSpec<T> =
-  | StatePropertyMatchSpecExact<T>
-  | StatePropertyMatchSpecOneOf<T>
-  | StatePropertyMatchSpecAllOf<T>;
-export interface StatePropertyMatchSpecExact<T> {
-  match: typeof MATCH_EXACT;
-  value: T;
-}
-export interface StatePropertyMatchSpecOneOf<T> {
-  match: typeof MATCH_ONE_OF;
-  values: ReadonlyArray<T>;
-}
-export interface StatePropertyMatchSpecAllOf<T> {
-  match: typeof MATCH_ALL_OF;
-  values: ReadonlyArray<T>;
-}
-
-export const MATCH_EXACT = "exact";
-export const MATCH_ONE_OF = "one_of";
-export const MATCH_ALL_OF = "all_of";
-
-export type GetState<
-  TStateValidation extends TStateValidationBase,
-  TStateSpec,
-> = {
-  [P in keyof TStateValidation & NonOptionalStateKeys<TStateSpec>]: t.TypeOf<
-    TStateValidation[P]["validation"]
-  >;
-} & {
-  [P in keyof TStateValidation &
-    Exclude<keyof TStateSpec, NonOptionalStateKeys<TStateSpec>>]?: t.TypeOf<
-    TStateValidation[P]["validation"]
-  >;
-};
-
-export type GetFullState<TStateValidation extends TStateValidationBase> =
-  GetState<TStateValidation, { [P in keyof TStateValidation]: true }>;
-
-export type NonOptionalStateKeys<T> = {
-  [P in keyof T]-?: false extends T[P] ? never : P;
-}[keyof T];
+export type GetStateValidationInfo<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TFactory extends CreateStateValidatorFactory<any>,
+> = TFactory extends CreateStateValidatorFactory<infer TStateValidation>
+  ? TStateValidation
+  : never;
 
 const errorsHaveKey = (errors: t.Errors, predicate: (key: string) => boolean) =>
   errors.some((error) => error.context.some(({ key }) => predicate(key)));

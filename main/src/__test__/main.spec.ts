7b0c52b257ff5aaf1b30eb417cac97a82d6ea56e
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-01-12 20:04:58 +0200

    #25 Hopefully fixing problem that caused process.…
    …exit not being properly called.

--- main/src/__test__/main.spec.ts
+++ main/src/__test__/main.spec.ts
@@ -16,13 +16,13 @@ test("Validate that erroneus main invocation works", async (c) => {
 
 test("Validate that normal main invocation with skipping process.exit call works", async (c) => {
   c.plan(1);
-  const info = await spec.invokeMainAndGetInfo(mainSuccess, false);
+  const info = await spec.invokeMainAndGetInfo(mainSuccess, true);
   c.deepEqual(info, { callProcessExit: false, exitCode: 0 });
 });
 
 test("Validate that erroneus main invocation with skipping process.exit call works", async (c) => {
   c.plan(1);
-  const info = await spec.invokeMainAndGetInfo(mainError, false);
+  const info = await spec.invokeMainAndGetInfo(mainError, true);
   // Notice that in case of errors, we always call process.exit
   c.deepEqual(info, { callProcessExit: true, exitCode: 1 });
 });
